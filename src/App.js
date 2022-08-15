e75ab9f0ca28f4cf8999572a99880de30b4b06c5
Maximiliano Molina <maximilianomolina1998@gmail.com>
2022-08-15 13:21:35 -0300

    subiendo proyecto por primera vez

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ramona Customs, prendas personalizadas.
         </p>
+        <a href="https://www.instagram.com/ramona_custom/">Seguinos</a>
         <a
           className="App-link"
           href="https://reactjs.org"
